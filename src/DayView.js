9fb8e301c22ec9a8a42519459851cb362f443ea0
mravimo <mikko.ravimo@gmail.com>
2018-05-18 12:50:38 +0300

    Fetch events for selected day

--- src/DayView.js
+++ src/DayView.js
@@ -7,20 +7,70 @@ class DayView extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            events: [],
+            events: {},
             stars: 0
         }
     }
 
-    componentDidMount() { 
-        this.firebaseRef = firebaseApp.database().ref('posts/' + 1 + '/starCount');
+    componentDidMount() {
+        this.linkToFirebase();
+    }
+
+    componentWillUnmount() {
+        // Un-register the listener on '/someData'.
+        this.firebaseRef.off('value', this.firebaseCallback);
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if(this.props.selectedDate !== prevProps.selectedDate) {
+            this.linkToFirebase();
+        }
+    }
+
+
+    render() {
+        return <div> 
+                <p>hello from day view - {this.props.selectedDate} - {this.state.stars   } </p> 
+                {this.events(this.state.events)}
+            </div>
+    }
+
+    linkToFirebase = () => {
+        
+        if(this.firebaseCallback) this.firebaseRef.off('value', this.firebaseCallback);
+        
+        var ref = 'events/' + this.props.selectedDate;
+        console.log("Fetching events: " + ref);
+        this.firebaseRef = firebaseApp.database().ref(ref);
         this.firebaseCallback = this.firebaseRef.on('value', (snap) => {      
-            this.setState({ stars: snap.val() });
+            console.log('Updating events from server: ' + snap.val());
+            this.setState({ events: snap.val() });
         });
     }
-    render() {
-        return <p>hello from day view - {this.props.selectedDate} - {this.state.stars   } </p>
+
+    events = (events) => {
+        var comps = []
+        for(var propKey in events) {
+            if(events.hasOwnProperty(propKey)) {
+                var eventData = events[propKey];
+                comps.push(<Event data={eventData} key={propKey} />);
+            }
+        }
+        return comps;
     }
 }
+
+
+
+class Event extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <p>hello from event {this.props.data.description}</p>
+    }
+}
+
 
 export default DayView
